9525e12b529053809008aae461acef8e6e6cd6b4
Jisu Kim <webmaster@alien.moe>
2021-03-08 06:01:33 +0900

    Add nothing message

--- printWakatime.sh
+++ printWakatime.sh
@@ -7,16 +7,24 @@ function print_row {
   printf "%-15s " $1
   printf "%-15s " "$2"
   util/progressbar.sh $3
- printf " %.1f%%\n" $3
+  printf " %.1f%%\n" $3
 }
+
+echo "📊 Weekly development breakdown"
 
 json_data=$(cat)
 
 LENGTH=$(echo $json_data | jq '.data.languages | length')
-LOOP=$(echo "min($LENGTH-1, 9)" | bc util/min.bc)
-for i in $(seq 0 $LOOP);do
-  NAME=$(echo $json_data | jq -r ".data.languages[$i].name")
-  TIMES=$(echo $json_data | jq -r ".data.languages[$i].text")
-  PERCENT=$(echo $json_data | jq -r ".data.languages[$i].percent")
-  print_row $NAME "$TIMES" $PERCENT
-done
+
+if [ $LENGTH -eq 0 ];then
+  echo "Nothing"
+else
+  LOOP=$(echo "min($LENGTH-1, 9)" | bc util/min.bc)
+  for i in $(seq 0 $LOOP);do
+    NAME=$(echo $json_data | jq -r ".data.languages[$i].name")
+    TIMES=$(echo $json_data | jq -r ".data.languages[$i].text")
+    PERCENT=$(echo $json_data | jq -r ".data.languages[$i].percent")
+    print_row $NAME "$TIMES" $PERCENT
+  done
+fi
+
--- test/wakatime.bats
+++ test/wakatime.bats
@@ -6,17 +6,18 @@ fixtures wakatime
 @test "Wakatime 0 language units" {
   run bash -c "cat $FIXTURE_ROOT/data.json | jq .zero_units | ./printWakatime.sh"
   [ $status -eq 0 ]
-  [ "$output" = "" ]
+  [ "${lines[0]}" = "📊 Weekly development breakdown" ]
+  [ "${lines[1]}" = "Nothing" ]
 }
 
 @test "Wakatime 5 language units" {
   run bash -c "cat $FIXTURE_ROOT/data.json | jq .five_units | ./printWakatime.sh"
   [ $status -eq 0 ]
-  [ ${#lines[@]} -eq 5 ]
+  [ ${#lines[@]} -eq 6 ]
 }
 
 @test "Wakatime language more than 10 units" {
   run bash -c "cat $FIXTURE_ROOT/data.json | jq .more_than_ten | ./printWakatime.sh"
   [ $status -eq 0 ]
-  [ ${#lines[@]} -eq 10 ]
+  [ ${#lines[@]} -eq 11 ]
 }
