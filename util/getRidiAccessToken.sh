5086c652c1a2b86d1722e7a6d69686c393c327d3
Jisu Kim <webmaster@alien.moe>
2021-03-10 06:15:45 +0900

    Refactoring

--- util/getRidiAccessToken.sh
+++ util/getRidiAccessToken.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-PHP_SESSION=$(curl -I https://ridibooks.com/account/action/login\?jsonp_callback=cb\&user_id=$RIDI_USER_ID\&password=$RIDI_USER_PW\&auto_login=0 \
-  2> /dev/null \
+PHP_SESSION=$(curl -s -I https://ridibooks.com/account/action/login\?jsonp_callback=cb\&user_id=$RIDI_USER_ID\&password=$RIDI_USER_PW\&auto_login=0 \
   | gawk 'match($0, /^set-cookie: PHPSESSID=([^;]+)/, ary) { print ary[1]; exit; }')
 
-curl --location -I "https://account.ridibooks.com/ridi/authorize/?client_id=$RIDI_CLIENT_ID&redirect_uri=https:%2F%2Faccount.ridibooks.com%2Fridi%2Fcomplete" \
-  --header "Cookie: PHPSESSID=$PHP_SESSION;" \
-  2> /dev/null \
+curl -L -s -I "https://account.ridibooks.com/ridi/authorize/?client_id=$RIDI_CLIENT_ID&redirect_uri=https:%2F%2Faccount.ridibooks.com%2Fridi%2Fcomplete" \
+  -H "Cookie: PHPSESSID=$PHP_SESSION;" \
   | gawk 'match($0, /^set-cookie: ridi-at=([^;]+)/, ary) { print ary[1]; exit; }'
