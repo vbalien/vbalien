d6fd4c85d68f4b44ef48d91c88280087299f245f
김지수(Jisu Kim) <webmaster@alien.moe>
2021-08-29 10:10:25 +0900

    Update getRidiAccessToken.sh

--- util/getRidiAccessToken.sh
+++ util/getRidiAccessToken.sh
@@ -3,6 +3,6 @@ RIDI_CLIENT_ID=$(curl -s https://ridibooks.com/account/login | gawk 'match($0, /
 PHP_SESSION=$(curl -s -I "https://ridibooks.com/account/action/login?jsonp_callback=cb&user_id=$RIDI_USER_ID&password=$RIDI_USER_PW&auto_login=0" \
   | gawk 'match($0, /^set-cookie: PHPSESSID=([^;]+)/, ary) { print ary[1]; exit; }')
 
-curl -L -s -I "https://account.ridibooks.com/ridi/authorize/?client_id=$RIDI_CLIENT_ID&redirect_uri=https:%2F%2Faccount.ridibooks.com%2Fridi%2Fcomplete" \
+curl -L -s -i "https://account.ridibooks.com/ridi/authorize/?client_id=$RIDI_CLIENT_ID&redirect_uri=https:%2F%2Faccount.ridibooks.com%2Fridi%2Fcomplete" \
   -H "Cookie: PHPSESSID=$PHP_SESSION;" \
   | gawk 'match($0, /^set-cookie: ridi-at=([^;]+)/, ary) { print ary[1]; exit; }'
